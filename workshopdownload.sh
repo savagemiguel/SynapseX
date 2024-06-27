98f92dbe416c92e45947cb524ba2222b0c21f35d
Miguel Savage <130864213+yotaboi@users.noreply.github.com>
2024-06-27 15:06:38 -0400

    Add files via upload
    This is a script that I have made to download workshop files from your workshop.txt file. This will automatically download the files for any server admins of QLDS. I have found that using your steamcmd and logging in with your steam account works far better with less issues than anonymous, however feel free to run as anonymous if you so choose. I guarantee nothing and this is as-is. I have taken bits and pieces from other scripts I've found on here to customize my own. There may be updates in the future to this.
    
    Change the two variables to match your install location and your steamcmd.sh install location.
    Also change steamID to your steamID you use to run your server. It may ask you for your 2FA (steam guard) if you have that setup.
    
    I wanted to try to make this into a plugin because minqlx's getmap.py does not work for me. I haven't been able to get it to work. I have converted this bash code into python so I may continue to try working on that as well. Ideally I wanted to have a similar function where you type in the workshop ID and it'll download it live on the server instead of doing it through a bash script.

---
+++ workshopdownload.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+timestamp() {
+  date +"%T"
+}
+
+me=`basename "$0"`
+echo "========== $me has started. =========="
+echo "========= $(date) ========="
+
+workshopFile='workshop.txt'
+steamCMD='YOUR STEAMCMD.SH PATH HERE'
+installLocation='YOUR QL INSTALL LOCATION HERE'
+workshopIDs=`cat $installLocation/baseq3/$workshopFile | grep -v '#' | sed '/^[ \t]*$/d'`
+numOfIDs=`echo "$workshopIDs" | wc -l`
+counter=0
+
+while [ $counter -lt $numOfIDs ]; do
+	currentID=`echo $workshopIDs | awk '{ print $1 }'`
+	workshopIDs=`echo $workshopIDs | cut -d ' ' -f2-`
+	if [ ! -d "$installLocation/steamapps/workshop/content/282440/$currentID" ]; then
+		echo -e "$(timestamp) Downloading item $(expr $counter + 1) of $numOfIDs from Steam with id $currentID "
+		$steamCMD/steamcmd.sh +force_install_dir $installLocation/ +login savageyota +workshop_download_item 282440 $currentID +quit
+		if [ $? -ne 0 ]; then
+			echo -e "$(timestamp) Download of id $currentID failed with steamcmd error code $? "
+		else
+			echo -e "Download of item id $currentID completed at $(timestamp) "
+    fi
+	else
+		echo -e "Steam directory for workshop item $currentID already exists, skipping. "
+	fi
+	((counter++))
+done
+echo "=== Completed $currentID/$numOfIDs on $(date) ==="
+echo "=== Please restart your server! ==="
+exit 0
